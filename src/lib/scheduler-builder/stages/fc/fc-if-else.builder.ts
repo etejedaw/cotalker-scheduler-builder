7d8a64f2fac9f04c0f4f22dab11f5e3de9387660
felipeinf <felipe.concha.a@mail.pucv.cl>
2025-04-08 17:48:55 -0400

    feat(fc): add FCIfElse builder stage

---
+++ src/lib/scheduler-builder/stages/fc/fc-if-else.builder.ts
@@ -0,0 +1,44 @@
+import { z } from "zod";
+import { StageBuilder } from "../../stage.builder";
+
+const FCIfElseSchema = z.object({
+	left: z.string(),
+	right: z.string()
+});
+type FCIfElseData = z.infer<typeof FCIfElseSchema>;
+
+interface NextConditionalStage {
+	IF: string;
+	ELSE: string;
+}
+
+export class FCIfElseBuilder extends StageBuilder<
+	FCIfElseData,
+	NextConditionalStage
+> {
+	constructor(key: string) {
+		super(key, "FCIfElse", null, FCIfElseSchema);
+		this.next.IF = "";
+		this.next.ELSE = "";
+	}
+
+	setRightCondition(condition: string): this {
+		this.data.right = condition;
+		return this;
+	}
+
+	setLeftCondition(condition: string): this {
+		this.data.left = condition;
+		return this;
+	}
+
+	setIf(key: string): this {
+		this.next.IF = key;
+		return this;
+	}
+
+	setElse(key: string): this {
+		this.next.ELSE = key;
+		return this;
+	}
+}
