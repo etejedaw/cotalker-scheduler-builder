9719e2fe7de5ff9f3a3b3698c0ea0fb7e5198ff5
felipeinf <felipe.concha.a@mail.pucv.cl>
2025-04-08 17:48:56 -0400

    feat(pb): add PBChangeState builder stage

---
+++ src/lib/scheduler-builder/stages/pb/pb-change-state.builder.ts
@@ -0,0 +1,42 @@
+import { z } from "zod";
+import { StageBuilder } from "../../stage.builder";
+
+const PBChangeStateSchema = z.object({
+	tid: z.string(),
+	smState: z.string(),
+	taskGroup: z.string()
+});
+type PBChangeStateData = z.infer<typeof PBChangeStateSchema>;
+
+export class PBChangeStateBuilder extends StageBuilder<PBChangeStateData> {
+	constructor(key: string) {
+		super(key, "PBChangeState", "2.1.0", PBChangeStateSchema);
+		this.next.ERROR = "";
+		this.next.SUCCESS = "";
+	}
+
+	setTaskId(taskId: string): this {
+		this.data.tid = taskId;
+		return this;
+	}
+
+	setSmState(smStateId: string): this {
+		this.data.smState = smStateId;
+		return this;
+	}
+
+	setTaskGroup(taskGroupId: string): this {
+		this.data.taskGroup = taskGroupId;
+		return this;
+	}
+
+	setSuccess(key: string): this {
+		this.next.SUCCESS = key;
+		return this;
+	}
+
+	setError(key: string): this {
+		this.next.ERROR = key;
+		return this;
+	}
+}
