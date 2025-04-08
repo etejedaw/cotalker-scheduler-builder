50cedb4b7438148fe18ff2dbd8cacd3144a678f6
felipeinf <felipe.concha.a@mail.pucv.cl>
2025-04-08 17:48:56 -0400

    chore: update stage builder exports

--- src/lib/scheduler-builder/index.ts
+++ src/lib/scheduler-builder/index.ts
@@ -1,8 +1,46 @@
-export * from "./stages/ccjs.builder";
-export * from "./stages/fc-if-else.builder";
-export * from "./stages/nw-request.builder";
-export * from "./stages/pb-change-state.builder";
-export * from "./stages/pb-channel-to-task-se.builder";
-export * from "./stages/pb-message.builder";
-export * from "./stages/pb-update-channel.builder";
 export * from "./scheduler.builder";
+
+export * from "./stages/ccjs/ccjs-v200.builder";
+export * from "./stages/ccjs/ccjs-v201.builder";
+export * from "./stages/ccjs/ccjs-v202.builder";
+export * from "./stages/ccjs/ccjs-v203.builder";
+export * from "./stages/ccjs/ccjs.builder";
+
+export * from "./stages/fc/fc-each-v200.builder";
+export * from "./stages/fc/fc-each-v300.builder";
+export * from "./stages/fc/fc-each.builder";
+export * from "./stages/fc/fc-if-else.builder";
+export * from "./stages/fc/fc-switch-all.builder";
+export * from "./stages/fc/fc-switch-one.builder";
+export * from "./stages/fc/fc-sleep.builder";
+
+export * from "./stages/nw/nw-bot-v2v3.builder";
+export * from "./stages/nw/nw-request-v200.builder";
+export * from "./stages/nw/nw-request.builder";
+
+export * from "./stages/pb/pb-action-button-v200.builder";
+export * from "./stages/pb/pb-action-button.builder";
+export * from "./stages/pb/pb-answer-checker-v200.builder";
+export * from "./stages/pb/pb-answer-checker.builder";
+export * from "./stages/pb/pb-calendar-v200.builder";
+export * from "./stages/pb/pb-change-state-v200.builder";
+export * from "./stages/pb/pb-change-state-v210.builder";
+export * from "./stages/pb/pb-change-state.builder";
+export * from "./stages/pb/pb-channel-add-user-v200.builder";
+export * from "./stages/pb/pb-channel-add-user.builder";
+export * from "./stages/pb/pb-channel-to-task-se-v200.builder";
+export * from "./stages/pb/pb-channel-to-task-se.builder"; // Prefers subfolder
+export * from "./stages/pb/pb-clean-channel-v200.builder";
+export * from "./stages/pb/pb-clean-channel.builder";
+export * from "./stages/pb/pb-copy-survey-v200.builder";
+export * from "./stages/pb/pb-copy-survey.builder";
+
+// Original PB Builders (if not duplicated in subfolder) - Check which ones remain unique to root
+// export * from "./stages/pb-message.builder"; // Check if pb/message.builder exists
+// export * from "./stages/pb-update-channel.builder"; // Check if pb/update-channel.builder exists
+
+// Note: Builders for 'pb/create-channel', 'pb/create-task', 'pb/duplicate-task', 'pb/get-channel', 
+// 'pb/get-task', 'pb/get-user', 'pb/task-add-editor', 'pb/update-property', 
+// 'pb/create-user', 'pb/update-task', 'util/delay', 'util/email-send', 
+// 'util/set-variable', 'api/request', 'forms/email-send' were previously deleted or not found.
+// They need to be created from masterdata JSON if required.
